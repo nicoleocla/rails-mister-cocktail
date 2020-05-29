8ce84b83542082067356dfd1f9e0b4295dfc409c
Nicole Ochoa <nochoa91@gmail.com>
2020-05-29 00:15:05 -0500

    Finished carousel and card grid display

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,11 @@ require("channels")
 // app/javascript/packs/application.js
 import 'bootstrap';
 
+import {dynamicSearchBar} from '../components/navbar';
+document.addEventListener('turbolinks:load', () => {
+  dynamicSearchBar();
+});
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
